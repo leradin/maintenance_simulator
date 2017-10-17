2cbcf2c9e09a7c16a199ef13825a5658dec6bd5e
leradin <lenin.lvrd@gmail.com>
2017-10-17 10:17:41 -0500

    Modelos de Exercise y Stage configurados con sus tablas pivote

---
+++ app/Exercise.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Exercise extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'exercises';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name','description'];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    public function stages(){
+        return $this->belongsToMany('\App\Stage','exercise_stage_pivot')
+            ->withPivot('stage_id');
+    }
+}
